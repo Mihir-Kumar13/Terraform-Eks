17be85ccb7bc1b3375c6de24644af35f65b83e92
MihirKumar <emailme.mihir@gmail.com>
2025-02-19 18:05:45 +0530

    update terraform files

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,7 @@
+region = "us-east-1"
+vpc_cidr = "10.0.0.0/16" 
+azs = [ "us-east-1" , "us-west-1" , "us-west-2" ]
+public_subnets = [  ]
+private_subnets = [  ]
+
+
---
+++ terraform/variable.tf
@@ -0,0 +1,26 @@
+variable "region" {
+    description = "value of region"
+  type = string
+}
+
+variable "vpc_cidr" {
+    description = "value of vpc cidr"
+  type = string
+}
+
+
+variable "azs" {
+    description = "value of availability zones"
+  type = list(string)
+}
+
+variable "private_subnets" {
+    description = "value of private subnets"
+  type = list(string)
+}
+
+variable "public_subnets" {
+    description = "value of public subnets"
+  type = list(string)
+}
+
